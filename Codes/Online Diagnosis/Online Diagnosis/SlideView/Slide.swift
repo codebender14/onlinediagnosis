4d2c17ec75463e1eecae9c12920a4410872aab72
codebender14 <111784806+codebender14@users.noreply.github.com>
2023-11-08 14:19:34 -0600

    cleaning up code base

--- Codes/Online Diagnosis/Online Diagnosis/SlideView/Slide.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class Slide: UIView {
-
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var continueBtn: UIButton!
-    @IBOutlet weak var labelDesc: UITextView!
-    
-}
